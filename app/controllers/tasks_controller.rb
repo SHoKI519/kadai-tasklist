e3b55f32c428ae9a754f3719b3718cdaac0d1da5
kimurashoki <kimurashoki0519@gmail.com>
2020-11-06 07:53:09 +0000

    final kadai

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,60 @@
+class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+      @tasks=Task.all
+  end
+
+  def show
+  end
+
+  def new
+      @task=Task.new
+  end
+
+  def create
+      @task=Task.new(task_params)
+
+      if @task.save
+          flash[:success] = 'タスクが正常に作成されました'
+          redirect_to @task
+
+     else 
+      flash[:danger] = 'タスクが作成されませんでした'
+      render :new
+     end
+  end
+
+  def edit
+  end
+
+  def update
+    
+      if @task.update(task_params)
+      flash[:success] = "タスクが編集されました"
+       redirect_to @task
+  else
+      flash.now[:danger]= 'タスクが編集されませんでした'
+       render :edit
+  end
+  end
+
+  def destroy
+      @task.destroy
+
+
+  flash[:success] = 'タスクが削除されました'
+  redirect_to tasks_url
+  end
+
+
+private
+
+ def set_task
+    @task = Task.find(params[:id])
+  end
+
+def task_params
+ params.require(:task).permit(:content, :status)
+end
+end
